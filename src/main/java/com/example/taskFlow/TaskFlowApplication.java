de85b5f1ebfefcab5ab0c53b65a6a0975d134fab
AgusMarconi <agustinamarconi39@gmail.com>
2025-06-01 23:07:24 -0300

    fixed Spring

--- src/main/java/com/example/taskFlow/TaskFlowApplication.java
+++ src/main/java/com/example/taskFlow/TaskFlowApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.taskFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TaskFlowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TaskFlowApplication.class, args);
 	}
 
 }

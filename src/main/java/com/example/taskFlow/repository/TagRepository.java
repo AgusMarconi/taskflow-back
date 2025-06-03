e1dc8a6c0bbf3b159caa417c11f96bae7fada873
Alejoqest <carpetaestudiantefabianalejo@proton.me>
2025-06-03 19:03:33 -0300

    fixed repositories

--- src/main/java/com/example/taskFlow/repository/TagRepository.java
+++ src/main/java/com/example/taskFlow/repository/TagRepository.java
@@ -1,9 +1,11 @@
 package com.example.taskFlow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.taskFlow.entity.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
 }
--- src/main/java/com/example/taskFlow/repository/TaskRepository.java
+++ src/main/java/com/example/taskFlow/repository/TaskRepository.java
@@ -1,9 +1,11 @@
 package com.example.taskFlow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.taskFlow.entity.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }

fdb07519ffae069bda8ebcafa92d0e478c4a08da
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-05 17:24:29 -0300

    Refactor TagDTO and TaskDTO classes to include Lombok annotations for automatic getter/setter generation and add fields for id, name, createdAt, updatedAt in TagDTO; enhance TaskDTO with fields for title, description, status, priority, created_at, updated_at, finished_at, and userId.

--- src/main/java/com/example/taskFlow/dto/TagDTO.java
+++ src/main/java/com/example/taskFlow/dto/TagDTO.java
@@ -1,5 +1,12 @@
 package com.example.taskFlow.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class TagDTO {
-    
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/example/taskFlow/dto/TaskDTO.java
+++ src/main/java/com/example/taskFlow/dto/TaskDTO.java
@@ -1,5 +1,19 @@
 package com.example.taskFlow.dto;
 
+import com.example.taskFlow.enumeration.Status;
+import com.example.taskFlow.enumeration.Priority;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class TaskDTO {
-    
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private Priority priority;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private LocalDateTime finished_at;
+    private Long userId; 
 }
